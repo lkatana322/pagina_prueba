e4fa6ecf01558923d4bd63839dde594f64ad83b8
lkatana322 <tercerosbran@gmail.com>
2024-08-27 15:37:09 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,25 @@
+// Obtener el modal
+var modal = document.getElementById("birthdayModal");
+
+// Obtener el botón que abre el modal
+var btn = document.querySelector('.btn-glow');
+
+// Obtener el elemento <span> que cierra el modal
+var span = document.getElementsByClassName("close")[0];
+
+// Cuando el usuario hace clic en el botón, abre el modal
+btn.onclick = function() {
+    modal.style.display = "flex";
+}
+
+// Cuando el usuario hace clic en <span> (x), cierra el modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Cuando el usuario hace clic fuera del modal, cierra el modal
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
